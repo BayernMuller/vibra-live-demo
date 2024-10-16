a7faa0a61a23cd5b4bef3f54c5b180e0cd078cc6
BayernMuller <cjjunk29@gmail.com>
2024-10-16 11:01:33 +0900

    add: shazam request

---
+++ shazam/shazam.py
@@ -0,0 +1,43 @@
+import requests
+import uuid
+import json
+import random
+from datetime import datetime
+from user_agent import USER_AGENT
+from timezone import TIMZONES
+
+
+class Shazam:
+    URL = "https://amp.shazam.com/discovery/v5/fr/FR/android/-/tag/"
+
+    def recognize(self, signature: str, samplems: int):
+        uuid1 = str(uuid.uuid4())
+        uuid2 = str(uuid.uuid4())
+        now_ms = int(datetime.now().timestamp() * 1000)
+        
+        url = self.URL + uuid1 + "/" + uuid2
+        url += "?sync=true&webv3=true&sampling=true&connected=&shazamapiversion=v3&sharehub=true&video=v3"
+        
+        body = {
+            "geolocation": {
+                "altitude": 300,
+                "latitude": 45,
+                "longitude": 2
+            },
+            "signature": {
+                "uri": signature,
+                "samplems": samplems,
+                "timestamp": now_ms
+            },
+            "timestamp": now_ms,
+            "timezone": random.choice(TIMZONES)
+        }
+
+        headers = {
+            "Content-Type": "application/json",
+            "User-Agent": random.choice(USER_AGENT),
+            "Content-Language": "en_US"
+        }
+
+        response = requests.post(url, headers=headers, data=json.dumps(body))
+        return response.json()
