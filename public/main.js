94501b4478171152758ce0947facb63b264cc115
BayernMuller <cjjunk29@gmail.com>
2024-10-17 08:02:47 +0900

    feat: implement recognizeSuccess and recognizeFailed functions to handle audio recognition results

--- public/main.js
+++ public/main.js
@@ -18,6 +18,31 @@ async function getPcmSignature(rawpcm, pcm_size, sampleRate, sampleWidth, channe
   }
 }
 
+function recognizeSuccess(album, title, artist, cover) {
+  document.getElementById('album').textContent = album;
+  document.getElementById('title').textContent = title;
+  document.getElementById('artist').textContent = artist;
+  document.getElementById('cover').src = cover;
+
+  const trackContainer = document.getElementById('track-container');
+  trackContainer.style.display = 'flex';
+
+  const titleContent = document.getElementById('title-content');
+  titleContent.style.display = 'none';
+
+  const centerContent = document.getElementById('center-content');
+  centerContent.style.display = 'none';
+
+  const errorMessage = document.getElementById('error-message');
+  errorMessage.style.display = 'none';
+}
+
+function recognizeFailed(error) {
+  const errorMessage = document.getElementById('error-message');
+  errorMessage.textContent = "Error: " + error;
+  errorMessage.style.display = 'block';
+}
+
 (async () => {
   const startBtn = document.getElementById('startBtn');
   const stopBtn = document.getElementById('stopBtn');
@@ -29,7 +54,7 @@ async function getPcmSignature(rawpcm, pcm_size, sampleRate, sampleWidth, channe
     const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
     audioContext = new AudioContext();
     const sourceNode = audioContext.createMediaStreamSource(stream);
-    await audioContext.audioWorklet.addModule('https://bayernmuller.github.io/vibra-live-demo/public/recorderProcessor.js');
+    await audioContext.audioWorklet.addModule('public/recorderProcessor.js');
     recorderNode = new AudioWorkletNode(audioContext, 'recorder-processor');
     recorderNode.port.onmessage = event => {
       const audioChunk = event.data;
@@ -50,14 +75,29 @@ async function getPcmSignature(rawpcm, pcm_size, sampleRate, sampleWidth, channe
     let audioBuffer = mergeBuffers(recordedChunks);
     const buffer_byte_length = audioBuffer.length * audioBuffer.BYTES_PER_ELEMENT;
     
-    getPcmSignature(audioBuffer, buffer_byte_length, 44100, 32, 1).then(signature => {
-      // get https://vercel-proxy-rust-three.vercel.app/api/shazam?uri= and samplems=
-      fetch(`https://vercel-proxy-rust-three.vercel.app/api/shazam?uri=${signature.uri}&samplems=${signature.samplems}`)
-        .then(response => response.json())
-        .then(data => {
-          document.getElementById('result').textContent = JSON.stringify(data);
-        });
-    });
+    getPcmSignature(audioBuffer, buffer_byte_length, 44100, 32, 1)
+      .then(signature => fetch(`https://vercel-proxy-rust-three.vercel.app/api/shazam?uri=${signature.uri}&samplems=${signature.samplems}`))
+      .then(response => response.json())
+      .then(data => {
+        try {
+          const album = data.track.sections[0].metadata[0].text;
+          const title = data.track.title;
+          const artist = data.track.subtitle;
+          const cover = data.track.images.coverart;
+          recognizeSuccess(album, title, artist, cover);
+        } catch (error) {
+          console.log(data);
+          console.log(error);
+          recognizeFailed(error.message);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        if (errorMessage) {
+          errorMessage.textContent = "Error: " + error.message;
+        }
+        recognizeFailed(error.message);
+      });
 
     startBtn.disabled = false;
     stopBtn.disabled = true;
