7612051841dfa5ad18404232ed209d450cd094ec
BayernMuller <cjjunk29@gmail.com>
2024-10-16 10:17:19 +0900

    style: remove commented-out line in recorderProcessor.js

--- public/recorderProcessor.js
+++ public/recorderProcessor.js
@@ -7,7 +7,6 @@ class RecorderProcessor extends AudioWorkletProcessor {
     const input = inputs[0];
     if (input.length > 0) {
       const channelData = input[0];
-      // 오디오 데이터를 메인 스레드로 전달
       this.port.postMessage(channelData.slice());
     }
     return true;
